5432b81dbb21a5741007428e74404337a12b7beb
kz6fittycent <jimmy.tigert@gmail.com>
2019-09-30 19:23:49 -0500

    Create suckatash.sh

---
+++ suckatash.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+clear
+echo ""
+echo ""
+echo "------------------------------------"
+echo ""
+echo "          UPDATES SUCKA!!!"
+echo ""
+echo "------------------------------------"
+echo ""
+echo ""
+sleep 3
+
+sudo apt update
+sudo apt dist-upgrade -y
+sudo apt autoremove -y
+sudo certbot renew
+sudo snap refresh
+echo ""
+echo ""
+echo "All Done!"
+echo ""
+sleep 3
+clear
